3d9a20a36f4a6d713ee0d35d1f3be47878368000
aschickhoff <achaville@hotmail.de>
2023-04-21 11:38:00 +0200

    updated application/user controller and app layout - added footer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  after_action :store_location
+
+  private
+
+  def store_location
+    session[:previous_url] = request.fullpath unless request.fullpath =~ /\/users/
+  end
+
+  def after_sign_in_path_for(resource)
+    session[:previous_url] || user_path(resource)
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    root_path
+  end
+  
 end
