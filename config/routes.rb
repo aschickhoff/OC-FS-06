621b4cd31863eb1b56cda11f73e7cc590ce39e36
aschickhoff <achaville@hotmail.de>
2023-05-07 14:02:13 +0200

    added username

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users , :controllers => {registrations: "registrations" }
   resources :activity_names
   resources :activities
   root "home#index"
